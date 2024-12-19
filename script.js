cd9c6523c0e1ef617e42700103206e18fafff0e8
Alexandru <alex.alexandru86@yahoo.ro>
2024-12-19 14:33:18 +0200

    Solar System 1.5

--- script.js
+++ script.js
@@ -26,3 +26,69 @@ function freezeTime() {
     bool = false;
   }
 }
+
+function menuButton(id) {
+  async function fetchData() {
+    const fetchInfo = await fetch("./data.json");
+    const response = await fetchInfo.json();
+
+    switch (id) {
+      case "menu_sun":
+        console.log(`debug apasand butonul ${response.sun.name}`);
+        document.querySelector("#other_side_img").src = response.sun.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.sun.name;
+        break;
+      case "menu_mercury":
+        console.log("debug apasand butonul mercury");
+        document.querySelector("#other_side_img").src = response.mercury.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.mercury.name;
+        break;
+      case "menu_venus":
+        console.log("debug apasand butonul venus");
+        document.querySelector("#other_side_img").src = response.venus.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.venus.name;
+        break;
+      case "menu_earth":
+        console.log("debug apasand butonul earth");
+        document.querySelector("#other_side_img").src = response.earth.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.earth.name;
+        break;
+      case "menu_mars":
+        console.log("debug apasand butonul mars");
+        document.querySelector("#other_side_img").src = response.mars.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.mars.name;
+        break;
+      case "menu_jupiter":
+        console.log("debug apasand butonul jupiter");
+        document.querySelector("#other_side_img").src = response.jupiter.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.jupiter.name;
+        break;
+      case "menu_saturn":
+        document.querySelector("#other_side_img").src = response.saturn.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.saturn.name;
+        break;
+      case "menu_uranus":
+        console.log("debug apasand butonul uranus");
+        document.querySelector("#other_side_img").src = response.uranus.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.uranus.name;
+        break;
+      case "menu_neptune":
+        console.log("debug apasand butonul neptune");
+        document.querySelector("#other_side_img").src = response.neptune.image;
+        document.querySelector("#other_size_heading").innerHTML =
+          response.neptune.name;
+        break;
+      default:
+        console.log("Please check again");
+    }
+  }
+  fetchData();
+}
