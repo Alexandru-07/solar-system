ea339cd9e8162c28fee87701c4a9d32272dae819
Alexandru <alex.alexandru86@yahoo.ro>
2024-12-26 15:53:48 +0200

    Solar System 3.5

--- intro/intro.js
+++ intro/intro.js
@@ -3,6 +3,23 @@ function videoSystem() {
 
   video.loop = true;
   video.play();
+  video.playbackRate = 0.7;
 }
 
 window.onload = videoSystem;
+
+function gotoMain() {
+  const headingM = document.querySelector(".heading");
+  const footerM = document.querySelector(".footer");
+  const list = [headingM, footerM];
+
+  for (let i = 0; i < list.length; i++) {
+    list[i].style.display = "none";
+  }
+
+  document.querySelector(".loading").style.display = "block";
+
+  setTimeout(() => {
+    window.location.href = "../index.html";
+  }, 3000);
+}
